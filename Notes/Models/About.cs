5f8a53e413cd4804dcd68b8f5b0455be39eb20cd
EleidyH <144007961+EleidyH@users.noreply.github.com>
2026-02-09 11:37:56 +0100

    link changed

--- Notes/Models/About.cs
+++ Notes/Models/About.cs
@@ -8,8 +8,6 @@ internal class About
 {
     public string Title => AppInfo.Name;
     public string Version => AppInfo.VersionString;
-    //urlhttps://aka.ms/maui
-    //urlhttps://youtu.be/Aq5WXmQQooo?list=RDAq5WXmQQooo
     public string MoreInfoUrl => "https://aka.ms/maui";
     public string Message => "This app is written in XAML and C# with .NET MAUI.";
 }
